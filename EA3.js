5e91a76801951e21666e7278790850f7ea5106e4
DBottin <95141321+DBottin@users.noreply.github.com>
2022-11-03 19:31:25 +0100

    Update EA3.js

--- EA3.js
+++ EA3.js
@@ -5,7 +5,7 @@ function makeColor() {
     var gl = canvas.getContext('experimental-webgl');
 
     // Hintergrundfarbe
-    gl.clearColor(0, 0, 0, 1);
+    gl.clearColor(0, 0, 0, 0.5);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     // Vertex Shader erstellen
@@ -43,18 +43,41 @@ function makeColor() {
     gl.useProgram(prog);
 
     //EA3 START
+    //Mittlere Figur
     var vertices = new Float32Array([
-        20, 1, 0,
-        0, 1, 0,
-        10, 15, 0]);
+        10, 10, 0,
+        5, 8, 0,
+        5, 12, 0,
+        8, 15, 0,
+        12, 15, 0,
+        15, 12, 0,
+        15, 8, 0,
+        12, 5, 0,
+        8, 5, 0]);
 
-    // Colors as rgba.
-    var colors = new Float32Array([1, 0, 0, 1,  0, 1, 0, 1,  0, 0, 1, 1,  0, 0, 0, 1]);
+    //Farben rgba
+    var colors = new Float32Array([
+        1, 0, 0, 1,
+        0, 1, 0, 1,
+        0, 0, 1, 1,
+        0, 1, 0, 1,
+        0, 0, 1, 1,
+        0, 1, 0, 1,
+        0, 0, 1, 1,
+        0, 1, 0, 1,
+        0, 0, 1, 1]);
 
-    // Index data.
-    var indices = new Uint16Array([0, 1, 2]);
+    //Eckdaten der Dreiecke
+    var indices = new Uint16Array([
+        0, 1, 2,
+        0, 2, 3,
+        0, 3, 4,
+        0, 4, 5,
+        0, 5, 6,
+        0, 6, 7,
+        0, 7, 8,
+        0, 8, 1]);
     //EA3 END
-
 
     var vbo = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
@@ -70,21 +93,118 @@ function makeColor() {
     gl.bindBuffer(gl.ARRAY_BUFFER, vboCol);
     gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
 
-    // Bind vertex buffer to attribute variable.
     var colAttrib = gl.getAttribLocation(prog, 'col');
     gl.vertexAttribPointer(colAttrib, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(colAttrib);
 
-
-    // Setup index buffer object.
     var ibo = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices,
-        gl.STATIC_DRAW);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
     ibo.numerOfEmements = indices.length;
 
-    gl.drawElements(gl.TRIANGLES, ibo.numerOfEmements,
-        gl.UNSIGNED_SHORT, 0);
+    gl.drawElements(gl.TRIANGLES, ibo.numerOfEmements, gl.UNSIGNED_SHORT, 0);
 
+    //Rahmen
+    var vertices = new Float32Array([
+        1, 1, 0,
+        3, 4, 0,
+        1, 7, 0,
+        3, 10, 0,
+        1, 13, 0,
+        3, 16, 0,
+        1, 19, 0,
+        4, 17, 0,
+        7, 19, 0,
+        10, 17, 0,
+        13, 19, 0,
+        16, 17, 0,
+        19, 19, 0,
+        17, 16, 0,
+        19, 13, 0,
+        17, 10, 0,
+        19, 7, 0,
+        17, 4, 0,
+        19, 1, 0,
+        16, 3, 0,
+        13, 1, 0,
+        10, 3, 0,
+        7, 1, 0,
+        4, 3, 0]);
+
+    var colors = new Float32Array([
+        1, 0, 0, 1,
+        1, 0, 1, 1,
+        0, 0, 1, 1,
+        0, 1, 1, 1,
+        0, 1, 0, 1,
+        1, 1, 0, 1,
+        1, 0, 0, 1,
+        1, 1, 0, 1,
+        0, 1, 0, 1,
+        0, 1, 1, 1,
+        0, 0, 1, 1,
+        1, 0, 1, 1,       
+        1, 0, 0, 1,
+        1, 0, 1, 1,
+        0, 0, 1, 1,
+        0, 1, 1, 1,
+        0, 1, 0, 1,
+        1, 1, 0, 1,
+        1, 0, 0, 1,
+        1, 1, 0, 1,
+        0, 1, 0, 1,
+        0, 1, 1, 1,
+        0, 0, 1, 1,
+        1, 0, 1, 1]);
+  
+    var indices = new Uint16Array([
+        0, 1, 2,
+        1, 2, 3,
+        2, 3, 4,
+        3, 4, 5,
+        4, 5, 6,
+        5, 6, 7,
+        6, 7, 8,
+        7, 8, 9,
+        8, 9, 10,
+        9, 10, 11,
+        10, 11, 12,
+        11, 12, 13,
+        12, 13, 14,
+        13, 14, 15,
+        14, 15, 16,
+        15, 16, 17,
+        16, 17, 18,
+        17, 18, 19,
+        18, 19, 20,
+        19, 20, 21,
+        20, 21, 22,
+        21, 22, 23,
+        22, 23, 0,
+        23, 0, 1]);
+  
+    var vbo = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    var posAttrib = gl.getAttribLocation(prog, 'pos');
+    gl.vertexAttribPointer(posAttrib, 3, gl.FLOAT, false, 0, 0); 
+    gl.enableVertexAttribArray(posAttrib);
+
+
+    var vboCol = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vboCol);
+    gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
+
+    var colAttrib = gl.getAttribLocation(prog, 'col');
+    gl.vertexAttribPointer(colAttrib, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(colAttrib);
+
+    var ibo = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+    ibo.numerOfEmements = indices.length;
+
+    gl.drawElements(gl.TRIANGLES, ibo.numerOfEmements, gl.UNSIGNED_SHORT, 0);
     //EA3 END
 }
