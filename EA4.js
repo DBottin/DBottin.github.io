0bf8394d8cb5b79e85a9efc10dee8365a89567f5
DBottin <95141321+DBottin@users.noreply.github.com>
2022-11-11 20:26:30 +0100

    Eigene EA4

--- EA4.js
+++ EA4.js
@@ -352,6 +352,216 @@ function makeShapes() {
             }
         }
     }
+
+
+    //Eigene Flaeche
+
+    var canvas = document.getElementById('canvas3');
+    var gl = canvas.getContext('experimental-webgl');
+
+    // Hintergrundfarbe
+    gl.clearColor(0, 0, 0, 0.5);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.frontFace(gl.CCW);
+    gl.enable(gl.CULL_FACE);
+    gl.cullFace(gl.BACK);
+
+    // Vertex Shader erstellen
+    var vsSource = '' +
+        'attribute vec3 pos;' +
+        'attribute vec4 col;' +
+        'varying vec4 color;' +
+        'void main(){' +
+        'color = col;' +
+        'gl_Position = vec4(0.05 * pos, 1);' +
+        '}';
+    var vs = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vs, vsSource);
+    gl.compileShader(vs);
+
+    // Fragment Shader erstellen
+    fsSouce = 'precision mediump float;' +
+        'varying vec4 color;' +
+        'void main() {' +
+        'gl_FragColor = color;' +
+        '}';
+    var fs = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fs, fsSouce);
+    gl.compileShader(fs);
+
+    // Programm erstellen
+    var prog = gl.createProgram();
+    gl.attachShader(prog, vs);
+    gl.attachShader(prog, fs);
+    gl.bindAttribLocation(prog, 0, "pos");
+    gl.linkProgram(prog);
+    gl.useProgram(prog);
+
+    var vertices, colors, indicesLines, indicesTris;
+    createOwn();
+
+    var vbo = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    var posAttrib = gl.getAttribLocation(prog, 'pos');
+    gl.vertexAttribPointer(posAttrib, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(posAttrib);
+
+
+    var vboCol = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vboCol);
+    gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
+
+    var colAttrib = gl.getAttribLocation(prog, 'col');
+    gl.vertexAttribPointer(colAttrib, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(colAttrib);
+
+
+    var iboLines = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iboLines);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indicesLines, gl.STATIC_DRAW);
+    iboLines.numberOfElements = indicesLines.length;
+
+    var iboTris = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iboTris);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indicesTris, gl.STATIC_DRAW);
+    iboTris.numberOfElements = indicesTris.length;
+
+
+    //Dreiecke zeichnen
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iboTris);
+    gl.drawElements(gl.TRIANGLES, iboTris.numberOfElements, gl.UNSIGNED_SHORT, 0);
+
+    //Linien zeichnen
+    gl.disableVertexAttribArray(colAttrib);
+    gl.vertexAttrib4f(colAttrib, 0, 0, 1, 1);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iboLines);
+    gl.drawElements(gl.LINES, iboLines.numberOfElements, gl.UNSIGNED_SHORT, 0);
+
+
+    //EA4 START
+    function createOwn() {
+        var n = 16;
+        var m = 16;
+
+        vertices = new Float32Array(3 * (n + 1) * (m + 1));
+        colors = new Float32Array(4 * (n + 1) * (m + 1));
+
+        indicesLines = new Uint16Array(2 * 2 * n * m);
+        indicesTris = new Uint16Array(3 * 2 * n * m);
+
+        var du = 4.5 / n;
+        var dv = 4 / m;
+
+        var iLines = 0;
+        var iTris = 0;
+
+        // Loop 1
+        for (var i = 0, u = -0.5; i <= n; i++, u += du) {
+
+            // Loop 2
+            for (var j = 0, v = 0; j <= m; j++, v += dv) {
+                var iVertex = i * (m + 1) + j;
+                var x = Math.pow(u, 3) / 3 - Math.pow(v, 2) / 2;
+                var y = u * Math.cos(v);
+                var z = Math.cos(u);
+                
+
+                //Vertices
+                vertices[iVertex * 3] = x;
+                vertices[iVertex * 3 + 1] = y;
+                vertices[iVertex * 3 + 2] = z;
+
+                //Farben
+                if (i % 9 == 0) {
+                    colors[iVertex * 4 + 0] = 0;
+                    colors[iVertex * 4 + 1] = 0;
+                    colors[iVertex * 4 + 2] = 1;
+                    colors[iVertex * 4 + 3] = 0.75;
+                }
+
+                if (i % 9 == 1) {
+                    colors[iVertex * 4 + 0] = 0;
+                    colors[iVertex * 4 + 1] = 0.5;
+                    colors[iVertex * 4 + 2] = 1;
+                    colors[iVertex * 4 + 3] = 1;
+                }
+
+                if (i % 9 == 2) {
+                    colors[iVertex * 4 + 0] = 0;
+                    colors[iVertex * 4 + 1] = 1;
+                    colors[iVertex * 4 + 2] = 1;
+                    colors[iVertex * 4 + 3] = 1;
+                }
+
+                if (i % 9 == 3) {
+                    colors[iVertex * 4 + 0] = 0;
+                    colors[iVertex * 4 + 1] = 1;
+                    colors[iVertex * 4 + 2] = 0.5;
+                    colors[iVertex * 4 + 3] = 1;
+                }
+
+                if (i % 9 == 4) {
+                    colors[iVertex * 4 + 0] = 0;
+                    colors[iVertex * 4 + 1] = 1;
+                    colors[iVertex * 4 + 2] = 0;
+                    colors[iVertex * 4 + 3] = 1;
+                }
+
+                if (i % 9 == 5) {
+                    colors[iVertex * 4 + 0] = 0;
+                    colors[iVertex * 4 + 1] = 1;
+                    colors[iVertex * 4 + 2] = 0.5;
+                    colors[iVertex * 4 + 3] = 1;
+                }
+
+                if (i % 9 == 6) {
+                    colors[iVertex * 4 + 0] = 0;
+                    colors[iVertex * 4 + 1] = 1;
+                    colors[iVertex * 4 + 2] = 1;
+                    colors[iVertex * 4 + 3] = 1;
+                }
+
+                if (i % 9 == 7) {
+                    colors[iVertex * 4 + 0] = 0;
+                    colors[iVertex * 4 + 1] = 0.5;
+                    colors[iVertex * 4 + 2] = 1;
+                    colors[iVertex * 4 + 3] = 1;
+                }
+
+                if (i % 9 == 8) {
+                    colors[iVertex * 4 + 0] = 0;
+                    colors[iVertex * 4 + 1] = 0;
+                    colors[iVertex * 4 + 2] = 1;
+                    colors[iVertex * 4 + 3] = 0.75;
+                }
+
+
+                //Indices
+                if (j > 0 && i > 0) {
+                    indicesLines[iLines++] = iVertex - 1;
+                    indicesLines[iLines++] = iVertex;
+                }
+                if (j > 0 && i > 0) {
+                    indicesLines[iLines++] = iVertex - (m + 1);
+                    indicesLines[iLines++] = iVertex;
+                }
+
+
+                if (j > 0 && i > 0) {
+                    indicesTris[iTris++] = iVertex;
+                    indicesTris[iTris++] = iVertex - 1;
+                    indicesTris[iTris++] = iVertex - (m + 1);
+                    //        
+                    indicesTris[iTris++] = iVertex - 1;
+                    indicesTris[iTris++] = iVertex - (m + 1) - 1;
+                    indicesTris[iTris++] = iVertex - (m + 1);
+                }
+            }
+        }
+    }
 }
 
 //EA4 END
